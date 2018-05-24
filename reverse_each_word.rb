83eff64bf0cf84405491e85e530cd03b90f372db
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 21:00:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   revstring = ""
   array = string.split(" ")
   array.collect do |x|
-    revstring << x.reverse
+    revstring << x.reverse << " "
   end
   return revstring
 end
